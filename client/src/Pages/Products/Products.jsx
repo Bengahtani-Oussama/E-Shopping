edb0670486c7820ab52a636b73607950b938e409
Bengahtani-Oussama <oussamabengahtani@gmail.com>
2023-04-23 16:42:12 +0100

    fix Errors

--- client/src/Pages/Products/Products.jsx
+++ client/src/Pages/Products/Products.jsx
@@ -1,74 +1,74 @@
-import React, { useState } from "react";
-import Accordion from "@mui/material/Accordion";
-import { AccordionDetails, AccordionSummary } from "@mui/material";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import { FakeProducts } from "../../Utils/FakeData";
-import { Link, useParams } from "react-router-dom";
-import { Carousel } from "react-carousel-minimal";
-import List from "../../Components/ListProduct/List";
-import UseFetch from "../../Hooks/UseFetch";
+import React, { useState } from 'react';
+import Accordion from '@mui/material/Accordion';
+import { AccordionDetails, AccordionSummary } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { FakeProducts } from '../../Utils/FakeData';
+import { Link, useParams } from 'react-router-dom';
+import { Carousel } from 'react-carousel-minimal';
+import List from '../../Components/ListProduct/List';
+import UseFetch from '../../Hooks/UseFetch';
 
 const size = [
-  "XS",
-  "S",
-  "M",
-  "L",
-  "XL",
-  "XXL",
-  "XXXL",
-  "2XL",
-  "3XL",
-  "4XL",
-  "5XL",
-  "6XL",
+  'XS',
+  'S',
+  'M',
+  'L',
+  'XL',
+  'XXL',
+  'XXXL',
+  '2XL',
+  '3XL',
+  '4XL',
+  '5XL',
+  '6XL',
 ];
 const Products = () => {
   // -----------------------------------
   const data_img = [
     {
       image:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/GoldenGateBridge-001.jpg/1200px-GoldenGateBridge-001.jpg",
-      caption: "San Francisco",
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/GoldenGateBridge-001.jpg/1200px-GoldenGateBridge-001.jpg',
+      caption: 'San Francisco',
     },
     {
       image:
-        "https://cdn.britannica.com/s:800x450,c:crop/35/204435-138-2F2B745A/Time-lapse-hyper-lapse-Isle-Skye-Scotland.jpg",
-      caption: "Scotland",
+        'https://cdn.britannica.com/s:800x450,c:crop/35/204435-138-2F2B745A/Time-lapse-hyper-lapse-Isle-Skye-Scotland.jpg',
+      caption: 'Scotland',
     },
     {
       image:
-        "https://static2.tripoto.com/media/filter/tst/img/735873/TripDocument/1537686560_1537686557954.jpg",
-      caption: "Darjeeling",
+        'https://static2.tripoto.com/media/filter/tst/img/735873/TripDocument/1537686560_1537686557954.jpg',
+      caption: 'Darjeeling',
     },
     {
       image:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Palace_of_Fine_Arts_%2816794p%29.jpg/1200px-Palace_of_Fine_Arts_%2816794p%29.jpg",
-      caption: "San Francisco",
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Palace_of_Fine_Arts_%2816794p%29.jpg/1200px-Palace_of_Fine_Arts_%2816794p%29.jpg',
+      caption: 'San Francisco',
     },
     {
       image:
-        "https://i.natgeofe.com/n/f7732389-a045-402c-bf39-cb4eda39e786/scotland_travel_4x3.jpg",
-      caption: "Scotland",
+        'https://i.natgeofe.com/n/f7732389-a045-402c-bf39-cb4eda39e786/scotland_travel_4x3.jpg',
+      caption: 'Scotland',
     },
     {
       image:
-        "https://www.tusktravel.com/blog/wp-content/uploads/2020/07/Best-Time-to-Visit-Darjeeling-for-Honeymoon.jpg",
-      caption: "Darjeeling",
+        'https://www.tusktravel.com/blog/wp-content/uploads/2020/07/Best-Time-to-Visit-Darjeeling-for-Honeymoon.jpg',
+      caption: 'Darjeeling',
     },
     {
       image:
-        "https://www.omm.com/~/media/images/site/locations/san_francisco_780x520px.ashx",
-      caption: "San Francisco",
+        'https://www.omm.com/~/media/images/site/locations/san_francisco_780x520px.ashx',
+      caption: 'San Francisco',
     },
     {
       image:
-        "https://images.ctfassets.net/bth3mlrehms2/6Ypj2Qd3m3jQk6ygmpsNAM/61d2f8cb9f939beed918971b9bc59bcd/Scotland.jpg?w=750&h=422&fl=progressive&q=50&fm=jpg",
-      caption: "Scotland",
+        'https://images.ctfassets.net/bth3mlrehms2/6Ypj2Qd3m3jQk6ygmpsNAM/61d2f8cb9f939beed918971b9bc59bcd/Scotland.jpg?w=750&h=422&fl=progressive&q=50&fm=jpg',
+      caption: 'Scotland',
     },
     {
       image:
-        "https://www.oyorooms.com/travel-guide/wp-content/uploads/2019/02/summer-7.jpg",
-      caption: "Darjeeling",
+        'https://www.oyorooms.com/travel-guide/wp-content/uploads/2019/02/summer-7.jpg',
+      caption: 'Darjeeling',
     },
   ];
 
@@ -94,35 +94,35 @@ const Products = () => {
   };
 
   return (
-    <div className="Categories my-2">
-      <div className="Cat-Container w-5/6 m-auto flex bg-white gap-5">
-        <div className="Cat-left flex-1 ">
-          <div className="sticky top-3">
-            <Accordion className="category">
+    <div className='Categories my-2'>
+      <div className='Cat-Container w-5/6 m-auto flex bg-white gap-5'>
+        <div className='Cat-left flex-1 '>
+          <div className='sticky top-3'>
+            <Accordion className='category'>
               <AccordionSummary
                 sx={{
-                  backgroundColor: "#D5AC4E",
+                  backgroundColor: '#D5AC4E',
                   maxHeight: 35,
                   minHeight: 5,
-                  "&.Mui-expanded": {
+                  '&.Mui-expanded': {
                     maxHeight: 35,
                     minHeight: 5,
                   },
                 }}
                 expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
+                aria-controls='panel1a-content'
+                id='panel1a-header'
               >
-                <p className="font-medium">Categories</p>
+                <p className='font-medium'>Categories</p>
               </AccordionSummary>
               <AccordionDetails>
                 {data?.map((cat) => (
-                  <div key={cat.id} className="p-1 flex gap-2">
+                  <div key={cat.id} className='p-1 flex gap-2'>
                     <input
-                      type="checkbox"
+                      type='checkbox'
                       value={cat.id}
                       id={cat.id}
-                      className="accent-blue-700"
+                      className='accent-blue-700'
                       onChange={handleChange}
                     />
                     <label htmlFor={cat.id}>{cat.attributes.subCatTitle}</label>
@@ -130,70 +130,70 @@ const Products = () => {
                 ))}
               </AccordionDetails>
             </Accordion>
-            <Accordion className="sort">
+            <Accordion className='sort'>
               <AccordionSummary
                 sx={{
-                  backgroundColor: "#D5AC4E",
+                  backgroundColor: '#D5AC4E',
                   maxHeight: 35,
                   minHeight: 5,
-                  "&.Mui-expanded": {
+                  '&.Mui-expanded': {
                     maxHeight: 35,
                     minHeight: 5,
                   },
                 }}
                 expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
+                aria-controls='panel1a-content'
+                id='panel1a-header'
               >
-                <p className="font-medium">Sort</p>
+                <p className='font-medium'>Sort</p>
               </AccordionSummary>
               <AccordionDetails>
-                <div className="flex gap-2">
+                <div className='flex gap-2'>
                   <input
-                    type="radio"
-                    value="ASC"
-                    name="radio"
-                    id="lowest"
-                    className="accent-blue-700"
-                    onChange={(e) => setSort("asc")}
+                    type='radio'
+                    value='ASC'
+                    name='radio'
+                    id='lowest'
+                    className='accent-blue-700'
+                    onChange={(e) => setSort('asc')}
                   />
-                  <label htmlFor="lowest">price (lowest first)</label>
+                  <label htmlFor='lowest'>price (lowest first)</label>
                 </div>
-                <div className="flex gap-2">
+                <div className='flex gap-2'>
                   <input
-                    type="radio"
-                    value="DESC"
-                    name="radio"
-                    id="highest"
-                    className="accent-blue-700"
-                    onChange={(e) => setSort("desc")}
+                    type='radio'
+                    value='DESC'
+                    name='radio'
+                    id='highest'
+                    className='accent-blue-700'
+                    onChange={(e) => setSort('desc')}
                   />
-                  <label htmlFor="highest">price (highest first)</label>
+                  <label htmlFor='highest'>price (highest first)</label>
                 </div>
               </AccordionDetails>
             </Accordion>
-            <Accordion className="price">
+            <Accordion className='price'>
               <AccordionSummary
                 sx={{
-                  backgroundColor: "#D5AC4E",
+                  backgroundColor: '#D5AC4E',
                   maxHeight: 35,
                   minHeight: 5,
-                  "&.Mui-expanded": {
+                  '&.Mui-expanded': {
                     maxHeight: 35,
                     minHeight: 5,
                   },
                 }}
                 expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
+                aria-controls='panel1a-content'
+                id='panel1a-header'
               >
-                <p className="font-medium">Price</p>
+                <p className='font-medium'>Price</p>
               </AccordionSummary>
               <AccordionDetails>
-                <div className="w-full flex justify-center items-center gap-3">
+                <div className='w-full flex justify-center items-center gap-3'>
                   <span>00</span>
                   <input
-                    type="range"
+                    type='range'
                     min={0}
                     max={2000}
                     onChange={(e) => setMaxPrice(e.target.value)}
@@ -202,32 +202,32 @@ const Products = () => {
                 </div>
               </AccordionDetails>
             </Accordion>
-            <Accordion className="brand">
+            <Accordion className='brand'>
               <AccordionSummary
                 sx={{
-                  backgroundColor: "#D5AC4E",
+                  backgroundColor: '#D5AC4E',
                   maxHeight: 35,
                   minHeight: 5,
-                  "&.Mui-expanded": {
+                  '&.Mui-expanded': {
                     maxHeight: 35,
                     minHeight: 5,
                   },
                 }}
                 expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
+                aria-controls='panel1a-content'
+                id='panel1a-header'
               >
-                <p className="font-medium">Brand</p>
+                <p className='font-medium'>Brand</p>
               </AccordionSummary>
               <AccordionDetails>
                 {data?.map((item, index) =>
                   item.attributes.products.data.map((brand) => (
-                    <div className="flex gap-2" key={brand.id}>
+                    <div className='flex gap-2' key={brand.id}>
                       <input
-                        type="checkbox"
+                        type='checkbox'
                         value={brand.id}
                         id={brand.id}
-                        className="accent-blue-700"
+                        className='accent-blue-700'
                       />
                       <label htmlFor={brand.id}>{brand.attributes.brand}</label>
                     </div>
@@ -235,63 +235,63 @@ const Products = () => {
                 )}
               </AccordionDetails>
             </Accordion>
-            <Accordion className="size">
+            <Accordion className='size'>
               <AccordionSummary
                 sx={{
-                  backgroundColor: "#D5AC4E",
+                  backgroundColor: '#D5AC4E',
                   maxHeight: 35,
                   minHeight: 5,
-                  "&.Mui-expanded": {
+                  '&.Mui-expanded': {
                     maxHeight: 35,
                     minHeight: 5,
                   },
                 }}
                 expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
+                aria-controls='panel1a-content'
+                id='panel1a-header'
               >
-                <p className="font-medium">Size</p>
+                <p className='font-medium'>Size</p>
               </AccordionSummary>
-              <AccordionDetails className="grid grid-cols-2">
+              <AccordionDetails className='grid grid-cols-2'>
                 {size.map((size, index) => (
-                  <div className="flex gap-2" key={index}>
+                  <div className='flex gap-2' key={index}>
                     <input
-                      type="checkbox"
+                      type='checkbox'
                       value={size}
-                      id="lowest"
-                      className="accent-blue-700"
+                      id='lowest'
+                      className='accent-blue-700'
                     />
                     <label htmlFor={size}>{size}</label>
                   </div>
                 ))}
               </AccordionDetails>
             </Accordion>
-            <Accordion className="ship">
+            <Accordion className='ship'>
               <AccordionSummary
                 sx={{
-                  backgroundColor: "#D5AC4E",
+                  backgroundColor: '#D5AC4E',
                   maxHeight: 35,
                   minHeight: 5,
-                  "&.Mui-expanded": {
+                  '&.Mui-expanded': {
                     maxHeight: 35,
                     minHeight: 5,
                   },
                 }}
                 expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
+                aria-controls='panel1a-content'
+                id='panel1a-header'
               >
-                <p className="font-medium">Ships From</p>
+                <p className='font-medium'>Ships From</p>
               </AccordionSummary>
-              <AccordionDetails className="grid grid-cols-2">
+              <AccordionDetails className='grid grid-cols-2'>
                 {data?.map((item, index) =>
                   item.attributes.products.data.map((shipping_from) => (
-                    <div className="flex gap-2" key={shipping_from.id}>
+                    <div className='flex gap-2' key={shipping_from.id}>
                       <input
-                        type="checkbox"
+                        type='checkbox'
                         value={shipping_from.id}
                         id={shipping_from.id}
-                        className="accent-blue-700"
+                        className='accent-blue-700'
                       />
                       <label htmlFor={shipping_from.id}>
                         {shipping_from.attributes.shipping_from}
@@ -303,27 +303,27 @@ const Products = () => {
             </Accordion>
           </div>
         </div>
-        <div className="Cat-right flex-[3] flex flex-col gap-5">
-          <div className="slide h-[150px]">
+        <div className='Cat-right flex-[3] flex flex-col gap-5'>
+          <div className='slide h-[150px]'>
             <Carousel
               data={data_img}
               time={2000}
-              width="100%"
-              height="150px"
+              width='100%'
+              height='150px'
               // captionStyle={captionStyle}
-              radius="10px"
+              radius='10px'
               slideNumber={true}
               // slideNumberStyle={slideNumberStyle}
-              captionPosition="bottom"
+              captionPosition='bottom'
               automatic={true}
               dots={true}
-              pauseIconColor="white"
-              pauseIconSize="40px"
-              slideBackgroundColor="gray"
-              slideImageFit="cover"
+              pauseIconColor='white'
+              pauseIconSize='40px'
+              slideBackgroundColor='gray'
+              slideImageFit='cover'
               // button img
               thumbnails={false}
-              thumbnailWidth="100px"
+              thumbnailWidth='100px'
               style={
                 {
                   // textAlign: "center",
@@ -334,7 +334,7 @@ const Products = () => {
               }
             />
           </div>
-          <div className="product">
+          <div className='product'>
             {/* <List /> */}
             <List
               catId={catId}
